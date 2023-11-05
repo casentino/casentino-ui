b27647ca4456835ba29631c06a059d4de90a53b3
casentino <opterk@gmail.com>
2023-11-06 02:34:57 +0900

    초기 프로젝트 구성 (jest, eslint, babel, workspace)

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const { defaults: tsjPreset } = require('ts-jest/presets');
+/** @type {import("ts-jest").JestConfigWithTsJest} */
+module.exports = {
+	preset: 'ts-jest/presets/default-esm',
+	testEnvironment: 'jsdom',
+	rootDir: '.',
+	moduleDirectories: ['node_modules', '<rootDir>/'],
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+	moduleFileExtensions: ['js', 'ts', 'tsx'],
+	transform: {
+		...tsjPreset.transform,
+		'^.+\\.[t|j]sx?$': 'babel-jest',
+	},
+};
